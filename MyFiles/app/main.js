a4895fe4a265a7c7c2c95a6cc81b2350ca89a60c
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2016-08-16 18:36:00 -0300

    importing http providers from angular2 for future http requests

--- MyFiles/app/main.js
+++ MyFiles/app/main.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './app.component', './media-item.service', 'angular2/core', './providers'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './app.component', './media-item.service', 'angular2/core', './providers', 'angular2/http'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, app_component_1, media_item_service_1, core_1, providers_1;
+    var browser_1, app_component_1, media_item_service_1, core_1, providers_1, http_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -18,11 +18,15 @@ System.register(['angular2/platform/browser', './app.component', './media-item.s
             },
             function (providers_1_1) {
                 providers_1 = providers_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             }],
         execute: function() {
             browser_1.bootstrap(app_component_1.AppComponent, [
                 media_item_service_1.MediaItemService,
-                core_1.provide(providers_1.LOOKUP_LISTS, { useValue: providers_1.lookupLists })
+                core_1.provide(providers_1.LOOKUP_LISTS, { useValue: providers_1.lookupLists }),
+                http_1.HTTP_PROVIDERS
             ]);
         }
     }
