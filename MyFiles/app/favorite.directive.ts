c9338699d5e02fa112f9a0bc0c52ca976e41b639
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2016-08-16 15:33:53 -0300

    added HostBindings and HostListeners for mouse hovering on favorite directive icons

--- MyFiles/app/favorite.directive.ts
+++ MyFiles/app/favorite.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding, Input} from 'angular2/core';
+import {Directive, HostBinding, HostListener, Input} from 'angular2/core';
 
 // mw is a directive prefix that we created for MediaWatcher, like Angular's 'ng'.
 @Directive({
@@ -7,6 +7,17 @@ import {Directive, HostBinding, Input} from 'angular2/core';
 
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.hovering = true;
+  }
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.hovering = false;
+  }
+
   @Input()
   set mwFavorite(value) {
     this.isFavorite = value;
